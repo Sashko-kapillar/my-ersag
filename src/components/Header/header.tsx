8abed449b26b8fbe7eb3e0cfa7e1ae2fb1a92964
kapillar <kapillarua@gmail.com>
2025-12-01 22:21:13 +0200

    style: add fonts

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -2,12 +2,12 @@
 // import Modal from '../ui/Modal/Modal';
 // import BookModal from '../BookModal/BookModal';
 // import BurgerToggle from '../BurgerToggle/BurgerToggle';
-// import SmartButton from '../ui/Button/SmartButton';
+import SmartButton from '@/components/ui/Button/SmartButton';
 // import HeaderNav from '../HeaderNav/HeaderNav';
 
 type HeaderProps = {
   setActiveIndex?: (index: number) => void;
-  onShowHome: () => void;
+  onShowHome?: () => void;
   isPrivacy?: boolean;
 };
 
@@ -23,12 +23,21 @@ const Header = ({
     <>
       <header className="bg-cod-gray/60 lg:bg-transparent relative z-100 w-full  lg:mt-10 3xl:mt-18 4xl:mt-17">
         <div className="section flex gap-4 py-3.5 1xl:py-0 max-h-17 1xl:h-14 4xl:h-16 h-full items-center justify-between">
-          <a href="/" className="w-1/3">
+          {/* <a href="/" className="w-1/3">
             <span className="sr-only">Повернутись на головну</span>
             <svg className="w-9.5 h-12.5 1xl:h-13 4xl:w-12 4xl:h-16.5 fill-tangerine">
               <use href="/images/svg/icons.svg#icon-logo" />
             </svg>
-          </a>
+          </a> */}
+          
+          <SmartButton
+          className="flex"
+              variant="primary"
+              label="На головну"
+              href="/"
+          >
+            
+          </SmartButton>
 
           {/* {isPrivacy ? (
             // Якщо сторінка політики — лише кнопка "На головну"
