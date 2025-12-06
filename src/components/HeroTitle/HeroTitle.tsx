63c3649ca99e2b184c4a4adf9cd22910da210478
kapillar <kapillarua@gmail.com>
2025-12-06 15:37:50 +0200

    style: change width for H1 to the brackpoints

--- src/components/HeroTitle/HeroTitle.tsx
+++ src/components/HeroTitle/HeroTitle.tsx
@@ -2,9 +2,9 @@ import SmartButton from "@/components/ui/Button/SmartButton";
 
 const HeroTitle = () => {
   return (    
-      <div className="flex gap-5.5 flex-col max-h-full h-full 1xl:pl-10 lg:max-w-[800px]">
+      <div className="flex gap-5.5 flex-col max-h-full h-full lg:pl-10 px-5 lg:px-10">
         {/* <VideoPlayer /> */}
-        <h1 className="font-second text-master-600">Природна турбота, що працює щодня</h1>
+        <h1 className="font-second xs:max-w-150 lg:max-w-150 1xl:max-w-200 text-master-60">Природна турбота, що працює щодня</h1>
         <p className="max-w-100 text-xl">М’який і природний догляд, створений для щоденного комфорту та гармонії</p>
         <SmartButton
             className="flex items-end text-xl lg:bg-master-500 lg:text-white hover:lg:bg-transparent hover:lg:text-chilean-fire hover:lg:border-chilean-fire transition-all duration-300 ease-out hover:-translate-y-0.5 hover:shadow-lg"
